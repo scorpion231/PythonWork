2ea4dea1a9f08f6c4d6414b65036cd716967234d
Gulshan Kumar Verma <33981412+scorpion231@users.noreply.github.com>
2020-05-27 17:47:56 +0530

    sum of positive integers

---
+++ variable1/variable1.py
@@ -0,0 +1,13 @@
+## sum of the first n positive integers
+
+
+##Formula used ((n)(n+1))/2
+
+##Read n values from user
+n=int(input("Enter a positive number : "))
+
+##compute the sum
+sum=n*(n+1)/2
+
+##display
+print("The sum of first ",n," positive integers is : ",sum)
