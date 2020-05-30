914908fa0a98ea715c8edc2e52949c6e7c11699e
Gulshan Kumar Verma <33981412+scorpion231@users.noreply.github.com>
2020-05-30 18:35:10 +0530

    Occurence of substring

---
+++ substring.py
@@ -0,0 +1,11 @@
+## find a substring in a string
+
+x=input(" Enter a string : ")
+a=x.lower()
+y=input(" Enter the substring you want to find : ")
+b=y.lower()
+count=a.count(b)
+print(count)
+
+
+
