f525262a9ec34d3655fc193b577502372cb12b70
Gulshan Kumar Verma <33981412+scorpion231@users.noreply.github.com>
2020-05-30 18:57:01 +0530

    Comments added

--- substring.py
+++ substring.py
@@ -1,10 +1,22 @@
 ## find a substring in a string
 
+
+##Taking input from the user
 x=input(" Enter a string : ")
+
+##Converting it to the lower case
 a=x.lower()
+
+##Taking substring as input which has to be searched
 y=input(" Enter the substring you want to find : ")
+
+##converting it to lowercase
 b=y.lower()
+
+##Checking the frequency
 count=a.count(b)
+
+##Printing the result
 print(count)
 
 
